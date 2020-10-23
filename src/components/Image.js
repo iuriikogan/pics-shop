debbf5c1b7c7faf12da995a166a4783372dd859d
koganiurii <koganiurii@gmail.com>
2020-10-23 18:07:56 +0300

    refactoring

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 const Image = ({ className, img }) => {
   const [hovered, setHovered] = useState(false);
 
+  const heartImage = hovered && <i className="ri-heart-line favorite" />;
+  const cartImage = hovered && <i className="ri-add-circle-line cart" />;
+
   return (
     <div
       onMouseEnter={() => setHovered(true)}
@@ -10,8 +13,8 @@ const Image = ({ className, img }) => {
       className={`${className} image-container`}
     >
       <img src={img.url} className="image-grid" />
-      {hovered ? <i className="ri-heart-line favorite" /> : null}
-      {hovered ? <i className="ri-add-circle-line cart" /> : null}
+      {heartImage}
+      {cartImage}
     </div>
   );
 };
